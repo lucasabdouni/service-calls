03515595f99f88fcfd9aedaddf74f097f0e797fe
Lucas Abdouni <lucasabdouni@hotmail.com>
2024-07-30 11:56:16 -0300

    feat: config env require

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,7 +1,8 @@
 import fastify from 'fastify';
+import { env } from './env';
 
 const app = fastify();
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
   console.log('Server listening on port 3333');
 });
