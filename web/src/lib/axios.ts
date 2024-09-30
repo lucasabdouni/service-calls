757262814e7f39d11ffbf7d112fa17b9f9d999de
Lucas Abdouni <lucasabdouni@hotmail.com>
2024-09-30 11:58:24 -0300

    feat: create arq environment

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,33 +1,37 @@
 import axios from 'axios';
+import { redirect } from 'react-router-dom';
+import { env } from '../env';
 
 const token = localStorage.getItem('token');
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: env.VITE_API_URL,
   headers: {
     Authorization: `Bearer ${token}`,
   },
 });
 
-api.interceptors.request.use(async (config) => {
-  await new Promise((resolve) =>
-    setTimeout(resolve, Math.round(Math.random() * 3000)),
-  );
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response?.status === 401) {
+      if (error.response?.data.code === 'token.expired') {
+        localStorage.removeItem('token');
+        redirect('/');
+      }
+    }
+    return Promise.reject(error);
+  },
+);
 
-  return config;
-});
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) =>
+      setTimeout(resolve, Math.round(Math.random() * 3000)),
+    );
 
-// api.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   (error) => {
-//     if (error.response?.status === 401) {
-//       if (error.response?.data.code === 'token.expired') {
-//         localStorage.removeItem('token');
-//         redirect('/');
-//       }
-//     }
-//     return Promise.reject(error);
-//   },
-// );
+    return config;
+  });
+}
