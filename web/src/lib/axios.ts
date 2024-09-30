d07cdaca18e77941138197ec03d4ccf22b18ebef
Lucas Abdouni <lucasabdouni@hotmail.com>
2024-09-30 11:50:55 -0300

    feat: implement lazzy in axios

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -1,26 +1,33 @@
 import axios from 'axios';
-import { redirect } from 'react-router-dom';
 
 const token = localStorage.getItem('token');
 
 export const api = axios.create({
-  baseURL: 'https://service-calls.onrender.com',
+  baseURL: 'http://localhost:3333',
   headers: {
     Authorization: `Bearer ${token}`,
   },
 });
 
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.response?.status === 401) {
-      if (error.response?.data.code === 'token.expired') {
-        localStorage.removeItem('token');
-        redirect('/');
-      }
-    }
-    return Promise.reject(error);
-  },
-);
+api.interceptors.request.use(async (config) => {
+  await new Promise((resolve) =>
+    setTimeout(resolve, Math.round(Math.random() * 3000)),
+  );
+
+  return config;
+});
+
+// api.interceptors.response.use(
+//   (response) => {
+//     return response;
+//   },
+//   (error) => {
+//     if (error.response?.status === 401) {
+//       if (error.response?.data.code === 'token.expired') {
+//         localStorage.removeItem('token');
+//         redirect('/');
+//       }
+//     }
+//     return Promise.reject(error);
+//   },
+// );
